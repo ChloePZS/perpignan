8caf997152f31408966f570933812fbb815705f4
ChloePZS <chloe-pozas@hotmail.fr>
2019-02-26 11:39:58 +0100

    bug on the script file so new script commit of figures script

---
+++ script2.R
@@ -0,0 +1,23 @@
+library(lme4)
+
+source ("R/analysis.R")
+source ("R/figures.R")
+
+########
+# DATA #
+########
+data       <-  read.csv('data/gapminder-FiveYearData.csv', stringsAsFactors = FALSE)
+data_1982  <-  data[data$year == 1982, ]
+
+############
+# ANALYSIS #
+############
+
+model  <-  lme4::lmer(log(lifeExp) ~ log(gdpPercap) + (1 + log(gdpPercap) | year), data = data)
+out    <-  coef(model)
+
+########
+# PLOT #
+########
+
+makefigure1(data_1982, output_filename = 'output/figure1.pdf')
